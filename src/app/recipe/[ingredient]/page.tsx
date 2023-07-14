4c8e02337941433eb09109a31b220c35cb3398b7
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2023-07-14 15:42:27 +0200

    error and loading pages included

--- src/app/recipe/[ingredient]/page.tsx
+++ src/app/recipe/[ingredient]/page.tsx
@@ -6,7 +6,16 @@ interface RecipeParams {
   params: { ingredient: string };
 }
 
-const getRecipe = async (ingredient: string) => {
+// interface RecipeData {
+//   id: string;
+//   title: string;
+//   description: string;
+//   image: string;
+//   category: string;
+//   cuisine: string;
+// }
+
+const getRecipeData = async (ingredient: string) => {
   const res = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=1&query=${ingredient}`
   );
@@ -15,24 +24,27 @@ const getRecipe = async (ingredient: string) => {
 };
 
 async function Recipe({ params }: RecipeParams) {
-  const recipeData = getRecipe(params.ingredient);
+  console.log("params.ingredient:", params.ingredient);
+  const recipeData = getRecipeData(params.ingredient);
   console.log("recipeData:", recipeData);
 
   const [recipeInfo] = await Promise.all([recipeData]);
   console.log("recipeInfo:", recipeInfo);
+
+  // console.log("recipeInfo results:", recipeInfo.results[0]);
   console.log("recipeInfo results:", recipeInfo.results);
 
-  const recipeSearched = JSON.stringify(recipeInfo.results);
+  // const recipeSearched = JSON.stringify(recipeInfo.results);
+  const recipeSearched = JSON.stringify(recipeInfo);
+  // const recipeSearched = recipeInfo.results[0];
   console.log("recipeSearched:", recipeSearched);
+  // console.log("recipeSearched:", recipeSearched);
 
   return (
-   
-
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
       <div className="p-8">
-       
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-        <Link href={"/"}>HOME</Link>
+          <Link href={"/"}>HOME</Link>
         </div>
 
         <div className="block mt-1 text-lg leading-tight font-medium text-black">
@@ -43,7 +55,7 @@ async function Recipe({ params }: RecipeParams) {
           {recipeSearched}
         </div>
 
-        
+        <div className="block mt-1 text-lg leading-tight font-medium text-black"></div>
       </div>
     </div>
   );
